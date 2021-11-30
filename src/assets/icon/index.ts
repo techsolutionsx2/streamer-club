ee58444472fa080a5f07f4ffec41c878e131a0da
techsolutionsx2 <benjaminmoritz8888@gmail.com>
2021-11-30 09:19:26 +0800

    fix: validate types

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -4,3 +4,5 @@ export { default as BellIcon } from "./bell";
 export { default as DownIcon } from "./down";
 export { default as UserIcon } from "./user";
 export { default as BeforeIcon } from "./before";
+export { default as ArrowsIcon } from "./arrows";
+export { default as CartIcon } from "./cart";
--- src/components/Slider/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as useSlider } from "./useSlider";
